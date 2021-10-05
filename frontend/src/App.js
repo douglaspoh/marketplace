34d37844c30be02c49e7ff211bd25e514dff87b4
douglaspoh <duckglas@hotmail.com>
2021-10-05 20:40:55 +0800

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import React, {useState, createContext} from 'react';
+import Home from './routes/Home';
+import Cart from './routes/Cart';
+import Login from './routes/Login';
+import NavBar from './components/NavBar';
+
+export const authContext = createContext();
 
 function App() {
+  
   return (
     <div className="App">
 
-
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/cart' component={Cart}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
