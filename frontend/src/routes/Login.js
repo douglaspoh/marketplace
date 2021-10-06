a1b7b578438ca9dea4d3d1fa13ccc21e0c56a097
Douglas <duckglas@hotmail.com>
2021-10-06 15:35:49 +0800

    login page form

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password,setPassword] = useState('');
+
+    const submit = () => {
+
+    }
+
     return (
-        <div>
-            Login
+        <div className='form'>
+            <form onSubmit={submit}>
+                <div className='formitem'>
+                    <label htmlFor='username'>Username: </label>
+                    <input type='text' onChange={(e)=>{setUsername(e.target.value)}} value={username} name='username'/>
+                </div>
+                <div className='formitem'>
+                    <label htmlFor='password'>Password: </label>
+                    <input type='text' onChange={(e)=>{setPassword(e.target.value)}} value={password} name='password'/>
+                </div>
+            </form>
         </div>
     )
 }
