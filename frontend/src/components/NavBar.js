e419e79d635edaa63eeb7a9af2296e7182f0c183
douglaspoh <duckglas@hotmail.com>
2021-10-16 23:10:52 +0800

    shopee design

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
 import cartlogo from '../img/cart.png';
+import shopeeicon from '../img/shopeeicon.jpg';
 
 function NavBar() {
     return (
         <div className='navbar'>
-            <NavLink to='/' exact={true} className='title'  style={{textDecoration:'none'}}> <b>Real Shopee</b> </NavLink>
+            <NavLink to='/' exact={true} className='title'  style={{textDecoration:'none'}}>
+                <img src={shopeeicon} className='shopeeicon' alt='icon'/><b>Real Shopee</b> 
+            </NavLink>
             <div className='navright'>
                 <NavLink to='/login' className='navitem' activeStyle={{fontWeight:'bold',color:'red'}} style={{textDecoration:'none'}}>Login</NavLink>
                 <div className='navitem'>|</div>
