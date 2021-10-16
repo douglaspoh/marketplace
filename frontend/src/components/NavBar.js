6013e624ab0d3da5aa3b5d39fb4a6964005cd591
douglaspoh <duckglas@hotmail.com>
2021-10-16 21:41:04 +0800

    styling navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,14 +5,14 @@ import cartlogo from '../img/cart.png';
 function NavBar() {
     return (
         <div className='navbar'>
-            <div className='title'> <b>Real Shopee</b> </div>
-            <div>
-            <NavLink to='/' exact={true} className='navitem' activeStyle={{fontWeight:'bold',color:'red'}} style={{textDecoration:'none'}}>Home</NavLink>
-            <NavLink to='/login' className='navitem' activeStyle={{fontWeight:'bold',color:'red'}} style={{textDecoration:'none'}}>Login</NavLink>
-            <NavLink to='/register' className='navitem' activeStyle={{fontWeight:'bold',color:'red'}} style={{textDecoration:'none'}}>Register</NavLink>
-            <NavLink to='/cart' exact={true} className='navitem' activeStyle={{fontWeight:'bold',color:'red'}} style={{textDecoration:'none'}}>
-                <img src={cartlogo} className='navcart' alt='cartpic'/>
-            </NavLink>
+            <NavLink to='/' exact={true} className='title'  style={{textDecoration:'none'}}> <b>Real Shopee</b> </NavLink>
+            <div className='navright'>
+                <NavLink to='/register' className='navitem' activeStyle={{fontWeight:'bold',color:'red'}} style={{textDecoration:'none'}}>Register</NavLink> 
+                <div className='navitem'>|</div>
+                <NavLink to='/login' className='navitem' activeStyle={{fontWeight:'bold',color:'red'}} style={{textDecoration:'none'}}>Login</NavLink>
+                <NavLink to='/cart' exact={true} className='navitem navcart' activeStyle={{fontWeight:'bold',color:'red'}} style={{textDecoration:'none'}}>
+                    <img src={cartlogo} className='navcart' alt='cartpic'/>
+                </NavLink>
             </div>
         </div>
     )
